29d8dc3f9feae31a0733109b3a334dff5ce9ae33
Manu Abraham <manukoshyabraham@gmail.com>
2018-10-11 21:41:59 +0530

    fix: expand message

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            Hey React Do!
+            Hey React Do! We believe in building from scratch.
          </div>
       );
    }
